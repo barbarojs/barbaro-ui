ddba5822a276ec6727a7613292bb536acfdedb11
Marco Sangalli <sangalli.marco@gmail.com>
2017-02-27 15:48:46 +0100

    some mods

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -11,14 +11,15 @@ const CSS_MAPS = ENV == 'development' && !cliArgs['no-sm'];
 
 module.exports = {
 	context: path.resolve(__dirname, 'src'),
-	
+
 	entry: [
 		'./index.js'
 	],
-	
+
 	output: {
 		path: path.resolve(__dirname, 'build'),
 		publicPath: '/',
+		compress: false,
 		filename: 'bundle.js',
 		chunkFilename: 'bundle-[id].js'
 	},
@@ -47,54 +48,61 @@ module.exports = {
 			loader: 'source-map'
 		}],
 		loaders: [{
-			test: /\.jsx?$/,
-			exclude: /node_modules/,
-			loader: 'babel'
-		},
-		{
-			test: /\.(scss|css)$/,
-			loaders: [
-				['css-loader?modules',
-				'localIdentName=[local]-[hash:base64:5]',
-				'importLoaders=1',
-				`sourceMap=${CSS_MAPS}`].join('&'),
-				`sass-loader?sourceMap=${CSS_MAPS}`,
-				'postcss-loader?parser=postcss-scss'
-			] 
-		},
-		{
-			test: /\.json$/,
-			loader: 'json'
-		}, {
-			test: /\.(xml|html|txt|md)$/,
-			loader: 'raw'
-		}, {
-			test: /\.(svg|woff2?|ttf|eot|jpe?g|png|gif)(\?.*)?$/i,
-			loader: ENV === 'production' ?
-				'file?name=[path][name]_[hash:base64:5].[ext]' :
-				'url'
-		}]
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: 'babel'
+			},
+			{
+				test: /\.(scss|css)$/,
+				loaders: [
+					['css-loader?modules',
+						'localIdentName=[local]-[hash:base64:5]',
+						'importLoaders=1',
+						`sourceMap=${CSS_MAPS}`
+					].join('&'),
+					`sass-loader?sourceMap=${CSS_MAPS}`,
+					'postcss-loader?parser=postcss-scss'
+				]
+			},
+			{
+				test: /\.json$/,
+				loader: 'json'
+			}, {
+				test: /\.(xml|html|txt|md)$/,
+				loader: 'raw'
+			}, {
+				test: /\.(svg|woff2?|ttf|eot|jpe?g|png|gif)(\?.*)?$/i,
+				loader: ENV === 'production' ?
+					'file?name=[path][name]_[hash:base64:5].[ext]' : 'url'
+			}
+		]
 	},
 
 	postcss: () => [
 		stylelint,
-		autoprefixer({ browsers: 'last 2 versions' })
+		autoprefixer({
+			browsers: 'last 2 versions'
+		})
 	],
 
 	plugins: ([
-		new webpack.NoErrorsPlugin(),
-		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify(ENV)
-		})
-	])
-	.concat(ENV === 'production' ?
-		[
+			new webpack.NoErrorsPlugin(),
+			new webpack.DefinePlugin({
+				'process.env.NODE_ENV': JSON.stringify(ENV)
+			})
+		])
+		.concat(ENV === 'production' ? [
 			// strip out babel-helper invariant checks
 			new ReplacePlugin([{
 				// this is actually the property name https://github.com/kimhou/replace-bundle-webpack-plugin/issues/1
 				partten: /throw\s+(new\s+)?[a-zA-Z]+Error\s*\(/g,
 				replacement: () => 'return;('
-			}])
+			}]),
+			new webpack.optimize.UglifyJsPlugin({
+				minimize: false,
+				compress: false,
+				mangle: false
+			})
 		] : []),
 	stats: {
 		colors: true
@@ -109,8 +117,7 @@ module.exports = {
 	},
 
 	devtool: ENV === 'production' ?
-		'source-map' :
-		'cheap-module-eval-source-map',
+		'' : 'cheap-module-eval-source-map',
 
 	devServer: {
 		port: process.env.PORT || 8080,
